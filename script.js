2b9e0681abaf11de9912a2a43a7ec1f275107f18
Noceolo <176486244+Noceolo@users.noreply.github.com.>
2025-01-14 17:01:28 +0100

    Added imageList array and query selector

---
+++ script.js
@@ -0,0 +1,5 @@
+imageList = ["image/image1.jpg","image/image2.jpg", "image/image3.jpg", "image/image4.jpg", "image/image5.jpg" ];
+
+prevBtn = document.getElementById("prev");
+nextBtn = document.getElementById("next");
+currentImg = document.getElementById("currentImage");
