864ebe8528a392c04dfac12a1faa40fc86e86b44
Noceolo <176486244+Noceolo@users.noreply.github.com.>
2025-01-20 11:17:01 +0100

    Added variables and const prefix

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+var i = 0;
 imageList = ["image/image1.jpg","image/image2.jpg", "image/image3.jpg", "image/image4.jpg", "image/image5.jpg" ];
 
-prevBtn = document.getElementById("prev");
-nextBtn = document.getElementById("next");
-currentImg = document.getElementById("currentImage");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+let currentImg = document.getElementById("currentImage");
+
+
+
+prevBtn.addEventListener("click", () =>{
+    currentImg.src = 
+
+})
