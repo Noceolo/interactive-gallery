a6161310bf85a0727d26ef65495b5f15c7a0edee
Noceolo <176486244+Noceolo@users.noreply.github.com.>
2025-01-20 13:01:35 +0100

    Added prev and next btn fcts

--- script.js
+++ script.js
@@ -3,11 +3,33 @@ imageList = ["image/image1.jpg","image/image2.jpg", "image/image3.jpg", "image/i
 
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
+
 let currentImg = document.getElementById("currentImage");
+prevBtn.disabled = true;
 
 
+prevBtn.addEventListener("click", () => {
+    if (i === 0){
+        console.log("image number" + i)
+        prevBtn.disabled = true;
+    }else {
+        i = i - 1
+        currentImg.src = imageList[i];
+        nextBtn.disabled = false;
+        if (i === 0) {
+            prevBtn.disabled = true
+        }
+    }
+})
 
-prevBtn.addEventListener("click", () =>{
-    currentImg.src = 
+nextBtn.addEventListener("click", () =>  {
+    if (i >= 0 && i < 4) {
+        i = i + 1
+        currentImg.src = imageList[i]
+        prevBtn.disabled = false
+        nextBtn.disabled = false
+    }if (i === 4){
+        nextBtn.disabled = true;
+    }
 
 })
